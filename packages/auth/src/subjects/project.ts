97987a1c725dac604e4b0a9385d85438f5b7dc6b
decobot <guileonidev@gmail.com>
2025-02-21 09:59:29 -0300

    feat: create subjects

--- packages/auth/src/subjects/project.ts
+++ packages/auth/src/subjects/project.ts
@@ -1,1 +1,14 @@
-export type ProjectSubject = ['create' | 'delete' | 'manage', 'Projects']
+import { z } from 'zod'
+
+export const projectSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('create'),
+    z.literal('update'),
+    z.literal('delete'),
+  ]),
+  z.literal('Projects'),
+])
+
+export type ProjectSubject = z.infer<typeof projectSubject>
