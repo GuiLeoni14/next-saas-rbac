798fe76dea69611c499fd339f0820ecb5129c22a
decobot <guileonidev@gmail.com>
2025-02-22 21:45:36 -0300

    feat: add swagger

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -10,6 +10,8 @@ export async function createAccount(app: FastifyInstance) {
     '/users',
     {
       schema: {
+        tags: ['auth'],
+        summary: "Create user's account",
         body: z.object({
           name: z.string(),
           email: z.string().email(),
