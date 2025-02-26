48cb0ae7d0cb46bff1e6d40c18ad94271a3fd4b5
decobot <guileonidev@gmail.com>
2025-02-26 15:39:03 -0300

    feat: reject and revoke invite

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -25,9 +25,12 @@ import { shutdownOrganization } from '@/http/routes/orgs/shutdown-organization'
 import { updateOrganization } from '@/http/routes/orgs/update-organization'
 
 import { createAccount } from './routes/auth/create-account'
+import { acceptInvite } from './routes/invites/accept-invite'
 import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
 import { getInvites } from './routes/invites/get-invites'
+import { rejectInvite } from './routes/invites/reject-invite'
+import { revokeInvite } from './routes/invites/revoke-invite'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -103,6 +106,9 @@ app.register(removeMember)
 app.register(createInvite)
 app.register(getInvite)
 app.register(getInvites)
+app.register(acceptInvite)
+app.register(rejectInvite)
+app.register(revokeInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
