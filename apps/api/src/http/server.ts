2455717d6d801849f5fde0d004471b2b42a41665
decobot <guileonidev@gmail.com>
2025-02-26 15:32:07 -0300

    feat: get invites

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -26,6 +26,8 @@ import { updateOrganization } from '@/http/routes/orgs/update-organization'
 
 import { createAccount } from './routes/auth/create-account'
 import { createInvite } from './routes/invites/create-invite'
+import { getInvite } from './routes/invites/get-invite'
+import { getInvites } from './routes/invites/get-invites'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -99,6 +101,8 @@ app.register(updateMember)
 app.register(removeMember)
 
 app.register(createInvite)
+app.register(getInvite)
+app.register(getInvites)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
