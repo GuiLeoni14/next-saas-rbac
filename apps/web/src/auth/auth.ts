49d6b16b4e3cc5b78346177dd9be17c52b8dd96e
decobot <guileonidev@gmail.com>
2025-03-06 14:41:31 -0300

    feat: add get user profile and auth route token

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,7 +1,28 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
   return await cookies().then((cookies) => {
     return !!cookies.get('token')?.value
   })
+}
+
+export async function auth() {
+  const token = await cookies().then((cookies) => {
+    return cookies.get('token')?.value
+  })
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+
+  try {
+    const { user } = await getProfile()
+
+    return { user }
+  } catch {}
+
+  redirect('/api/auth/sign-out')
 }
